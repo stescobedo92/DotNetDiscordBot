fbb0da9fc2e6ace523ca9431776eeed1e164f043
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-03-19 15:59:32 -0600

    Add tests

--- DotNetDiscordBotTest/DotNetDiscordBot.cs
+++ DotNetDiscordBotTest/DotNetDiscordBot.cs
@@ -1,16 +1,72 @@
-namespace DotNetDiscordBotTest
-{
-    public class DotNetDiscordBotTest
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+using Discord;
+using Discord.WebSocket;
+using DotNetDiscordBot;
+using Moq;
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+namespace DotNetDiscordBotTest;
+
+[TestFixture]
+public class DotNetDiscordBotTest
+{
+    private readonly DiscordSocketClient? _mockClient;
+    private readonly DiscordClientUtilities? _clientUtilities;
+
+    [SetUp]
+    public void Setup() => Setup(_mockClient, _clientUtilities);
+    
+    public void Setup(DiscordSocketClient mockClient, DiscordClientUtilities clientUtilities)
+    {
+        mockClient = new Mock<DiscordSocketClient>().Object;
+        clientUtilities = new DiscordClientUtilities();
+    }
+
+    [Test]
+    public async Task StartBotAsync_ClientLoginIsCalled()
+    {
+        // Act
+        await _clientUtilities.StartBotAsync();
+
+        var mockClientLogin = _mockClient?.LoginAsync(TokenType.Bot, It.IsAny<string>());
+
+        // Assert
+        Mock.Get(_mockClient).Verify(mockClientLogin, Times.Once);
+    }
+
+    [Test]
+    public async Task StartBotAsync_ClientStartAsyncIsCalled()
+    {
+        // Act
+        await _clientUtilities.StartBotAsync();
+
+        // Assert
+        Mock.Get(_mockClient).Verify(c => c.StartAsync(), Times.Once);
+    }
+
+    [Test]
+    public async Task MessageHandler_WhenAuthorIsBot_NoReplyIsSent()
+    {
+        // Arrange
+        var mockMessage = new Mock<SocketMessage>();
+        mockMessage.Setup(m => m.Author.IsBot).Returns(true);
+
+        // Act
+        await _clientUtilities.MessageHandler(mockMessage.Object);
+
+        // Assert
+        Mock.Get(_mockClient).Verify(c => c.SendMessageAsync(It.IsAny<SocketMessage>(), It.IsAny<string>()), Times.Never);
+    }
+
+    [Test]
+    public async Task MessageHandler_WhenAuthorIsNotBot_ReplyIsSent()
+    {
+        // Arrange
+        var mockMessage = new Mock<SocketMessage>();
+        mockMessage.Setup(m => m.Author.IsBot).Returns(false);
+
+        // Act
+        await _clientUtilities.MessageHandler(mockMessage.Object);
+
+        // Assert
+        Mock.Get(_mockClient).Verify(c => c.SendMessageAsync(It.IsAny<SocketMessage>(), "C# response works!"), Times.Once);
     }
 }
