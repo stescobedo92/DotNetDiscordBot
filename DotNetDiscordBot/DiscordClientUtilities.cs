8fdf5bf5e9728779849fec597606a727cbd1fe53
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-03-19 15:43:10 -0600

    Remove uneccesary code and namespaces

--- DotNetDiscordBot/DiscordClientUtilities.cs
+++ DotNetDiscordBot/DiscordClientUtilities.cs
@@ -1,42 +1,36 @@
 ﻿using Discord;
 using Discord.WebSocket;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNetDiscordBot
+namespace DotNetDiscordBot;
+
+public class DiscordClientUtilities
 {
-    public class DiscordClientUtilities
+    private readonly DiscordSocketClient client;
+    private const string TOKEN = "ADD_TOKEN_HERE";
+
+    public DiscordClientUtilities()
     {
-        private readonly DiscordSocketClient client;
-        private const string TOKEN = "ADD_TOKEN_HERE";
-
-        public DiscordClientUtilities()
-        {
-            this.client = new DiscordSocketClient();
-            this.client.MessageReceived += MessageHandler;
-        }
-
-        public async Task StartBotAsync()
-        {
-
-            this.client.Log += LogFuncAsync;
-            await this.client.LoginAsync(TokenType.Bot, TOKEN);
-            await this.client.StartAsync();
-            await Task.Delay(-1);
-
-            Task LogFuncAsync(LogMessage message) { Console.Write(message.ToString()); return Task.CompletedTask; }
-        }
-
-        private async Task MessageHandler(SocketMessage message)
-        {
-            if (message.Author.IsBot) return;
-
-            await ReplyAsync(message, "C# response works!");
-        }
-
-        private async Task ReplyAsync(SocketMessage message, string response) => await message.Channel.SendMessageAsync(response);
+        this.client = new DiscordSocketClient();
+        this.client.MessageReceived += MessageHandler;
     }
+
+    public async Task StartBotAsync()
+    {
+
+        this.client.Log += LogFuncAsync;
+        await this.client.LoginAsync(TokenType.Bot, TOKEN);
+        await this.client.StartAsync();
+        await Task.Delay(-1);
+
+        Task LogFuncAsync(LogMessage message) { Console.Write(message.ToString()); return Task.CompletedTask; }
+    }
+
+    private async Task MessageHandler(SocketMessage message)
+    {
+        if (message.Author.IsBot) return;
+
+        await ReplyAsync(message, "C# response works!");
+    }
+
+    private async Task ReplyAsync(SocketMessage message, string response) => await message.Channel.SendMessageAsync(response);
 }
