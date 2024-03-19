6bd3afe5be4faea28dbe2229516db996bd37d9dc
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-03-19 15:31:45 -0600

    Add constructor and private declarations

---
+++ DotNetDiscordBot/DiscordClientUtilities.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetDiscordBot
+{
+    public class DiscordClientUtilities
+    {
+        private readonly DiscordSocketClient client;
+        private const string TOKEN = "ADD_TOKEN_HERE";
+
+        public DiscordClientUtilities()
+        {
+            this.client = new DiscordSocketClient();
+            this.client.MessageReceived += MessageHandler;
+        }
+    }
+}
