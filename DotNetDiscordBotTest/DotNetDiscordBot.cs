7cfd94dfff911b0a23b6f8b6a8a6d431850ada12
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-03-19 15:45:56 -0600

    Add unit test

---
+++ DotNetDiscordBotTest/DotNetDiscordBot.cs
@@ -0,0 +1,16 @@
+namespace DotNetDiscordBotTest
+{
+    public class DotNetDiscordBotTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
