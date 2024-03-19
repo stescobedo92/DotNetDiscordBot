a96fc0b81d9edd7a9a4971755df154cf6f25773d
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-03-19 15:33:21 -0600

    Add StartBotAsync and MessageHandler functions

--- DotNetDiscordBot/DiscordClientUtilities.cs
+++ DotNetDiscordBot/DiscordClientUtilities.cs
@@ -18,5 +18,25 @@ namespace DotNetDiscordBot
             this.client = new DiscordSocketClient();
             this.client.MessageReceived += MessageHandler;
         }
+
+        public async Task StartBotAsync()
+        {
+
+            this.client.Log += LogFuncAsync;
+            await this.client.LoginAsync(TokenType.Bot, TOKEN);
+            await this.client.StartAsync();
+            await Task.Delay(-1);
+
+            Task LogFuncAsync(LogMessage message) { Console.Write(message.ToString()); return Task.CompletedTask; }
+        }
+
+        private async Task MessageHandler(SocketMessage message)
+        {
+            if (message.Author.IsBot) return;
+
+            await ReplyAsync(message, "C# response works!");
+        }
+
+        private async Task ReplyAsync(SocketMessage message, string response) => await message.Channel.SendMessageAsync(response);
     }
 }
